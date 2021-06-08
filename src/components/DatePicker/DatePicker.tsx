49a6c11d3d8de3e87ff815fb58251a8121ae404e
navarrojandg <navarrojanderrick@gmail.com>
2021-06-08 16:49:18 -0700

    feat/ update DatePicker

--- src/components/DatePicker/DatePicker.tsx
+++ src/components/DatePicker/DatePicker.tsx
@@ -1,38 +1,36 @@
 import React from 'react';
 
 import MomentUtils from '@date-io/moment';
-import { MuiPickersUtilsProvider, KeyboardDatePicker } from '@material-ui/pickers';
+import { MuiPickersUtilsProvider, KeyboardDatePicker, KeyboardDatePickerProps } from '@material-ui/pickers';
 import { BaseKeyboardPickerProps } from '@material-ui/pickers/_shared/hooks/useKeyboardPickerState';
 import moment, { Moment } from 'moment';
 
-export interface DatePickerProps {
-    id: string;
-    label: string;
-    value: Moment;
-    onChange: BaseKeyboardPickerProps['onChange'];
-};
+// export interface DatePickerProps {
+//     id: string;
+//     label: string;
+//     value: Moment;
+//     onChange: BaseKeyboardPickerProps['onChange'];
+// };
+
+export type DatePickerProps = KeyboardDatePickerProps;
 
 export const minDate = moment().subtract(1, 'year');
 
 export default function DatePicker(props: DatePickerProps) {
-    let { id, label, value, onChange } = props;
-
     return (
         <MuiPickersUtilsProvider utils={MomentUtils}>
             <KeyboardDatePicker
-                disableToolbar
+                {...props}
                 variant="inline"
                 format="MM/DD/YYYY"
                 margin="normal"
-                id={id}
-                label={label}
-                value={value}
-                onChange={onChange}
                 KeyboardButtonProps={{
                     'aria-label': 'change-date'
                 }}
-                disableFuture
+                disableFuture={true}
+                disableToolbar={true}
                 minDate={minDate}
+                data-testid="datepicker"
             />
         </MuiPickersUtilsProvider>
     );
