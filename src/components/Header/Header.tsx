41f5cec6c3cbdbcf79954a0b7b65af2671c15a79
birdsnbears <louischu15@gmail.com>
2021-06-18 14:47:41 -0700

    removed unecessary Grid container in header. woops

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,11 +6,9 @@ export default function Header() {
 
   return <React.Fragment>
     <Grid container>
-      <Grid item>
-        <Grid item className="mockstock-logo">
-          <img src="MockStockLogo.png" alt="" />
-          <h5 className="subtitle">Official Stock Market Simulator</h5>
-        </Grid>
+      <Grid item className="mockstock-logo">
+        <img src="MockStockLogo.png" alt="" />
+        <h5 className="subtitle">Official Stock Market Simulator</h5>
       </Grid>
     </Grid>
   </React.Fragment>;
