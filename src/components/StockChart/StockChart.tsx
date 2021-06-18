0d2d764f6cafc15f0eb756a2a42602bfacf6da60
birdsnbears <louischu15@gmail.com>
2021-06-18 14:50:39 -0700

    Made Candlestick bg white and rounded border

--- src/components/StockChart/StockChart.tsx
+++ src/components/StockChart/StockChart.tsx
@@ -1,74 +1,76 @@
 import React from 'react';
 import { ActiveInvestmentContext } from '../../contexts/ActiveInvestmentContext';
 import ReactApexChart from "react-apexcharts";
+import "./StockChart.css";
 
 export default function StockChart() {
-    const { activeInvestment } = React.useContext(ActiveInvestmentContext);
-    const { candles } = activeInvestment;
+  const { activeInvestment } = React.useContext(ActiveInvestmentContext);
+  const { candles } = activeInvestment;
 
-    const data = []
-    if(candles) {
-        var i
-        for(i = 0; i < candles["t"].length; i++) {
-            const x = new Date(candles["t"][i] * 1000)
-            const y0 = candles["o"][i]
-            const y1 = candles["h"][i]
-            const y2 = candles["l"][i]
-            const y3 = candles["c"][i]
+  const data = []
+  if (candles) {
+    var i
+    for (i = 0; i < candles["t"].length; i++) {
+      const x = new Date(candles["t"][i] * 1000)
+      const y0 = candles["o"][i]
+      const y1 = candles["h"][i]
+      const y2 = candles["l"][i]
+      const y3 = candles["c"][i]
 
-            const y = []
-            y.push(y0)
-            y.push(y1)
-            y.push(y2)
-            y.push(y3)
+      const y = []
+      y.push(y0)
+      y.push(y1)
+      y.push(y2)
+      y.push(y3)
 
-            const ob = {
-                x: x,
-                y: y
+      const ob = {
+        x: x,
+        y: y
+      }
+
+      data.push(ob)
+    }
+  }
+
+  const obk = {
+    data: data
+  }
+
+  var series = []
+  series.push(obk)
+
+  return (
+    <div id="chart">
+      <ReactApexChart
+        options={
+          {
+            chart: {
+              type: 'candlestick',
+              height: 350
+            },
+            title: {
+              text: 'CandleStick Chart',
+              align: 'left'
+            },
+            xaxis: {
+              type: 'datetime'
+            },
+            yaxis: {
+              tooltip: {
+                enabled: true
+              },
+              labels: {
+                formatter: function (val, index) {
+                  return val.toFixed(2);
+                }
+              }
             }
-                
-            data.push(ob)
+          }
         }
-    }
-
-    const obk = {
-        data: data
-    }
-
-    var series = []
-    series.push(obk)
-
-    return (
-        <div id="chart">
-            <ReactApexChart 
-                options={
-                    {
-                        chart: {
-                            type: 'candlestick',
-                            height: 350
-                        },
-                        title: {
-                            text: 'CandleStick Chart',
-                            align: 'left'
-                        },
-                        xaxis: {
-                            type: 'datetime'
-                        },
-                        yaxis: {
-                            tooltip: {
-                                enabled: true
-                            },
-                            labels: {formatter: function(val, index) {
-                                return val.toFixed(2);
-                              }
-                            }
-                        }
-                    }
-                }  
-                series={series} type="candlestick" height={350} 
-            />
-        </div>
-    )    
+        series={series} type="candlestick" height={350}
+      />
+    </div>
+  )
 };
 
 // NOT SURE WHAT TO DO WITH THIS CODE BELOW, CAME WITH ApexChart
