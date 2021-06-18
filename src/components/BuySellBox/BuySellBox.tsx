185726899346a91f65d964582b29a4ceb3ff9713
MatthewGHub2 <matthew.chalifoux.641@my.csun.edu>
2021-06-18 12:58:44 -0700

    buyButton changing liquidBalance and updating display properly

--- src/components/BuySellBox/BuySellBox.tsx
+++ src/components/BuySellBox/BuySellBox.tsx
@@ -81,7 +81,7 @@ export default function InputSlider() {
             token,
         }); 
         console.log(trades)
-    }, [activeInvestment.stock])
+    }, [activeInvestment.stock, liquidBalance.curr])
 
 
     const onClick = async () => {
@@ -125,6 +125,16 @@ export default function InputSlider() {
                 ...trades,
                 items: [trade, ...trades.items]
             })
+
+            let hold2 = currency(liquidBalance.curr.value - buyAmount.value)
+
+            updateLiquidBalance({
+                ...liquidBalance,
+                prev: liquidBalance.curr,
+                curr: currency(liquidBalance.curr.value - buyAmount.value)
+            })
+
+            setValue(!hold2 ? '' : Number(hold2))
  
         } catch(error) {
             errorHandler(error);
@@ -256,168 +266,3 @@ export default function InputSlider() {
         </div>
     );
 }
-
-
-
-
-
-
-
-
-
-// import React, {useEffect} from 'react';
-// import moment, { Moment } from 'moment';
-// import { ActiveInvestmentContext } from '../../contexts/ActiveInvestmentContext';
-// import { TradesContext } from '../../contexts/TradesContext';
-// import SymbolBox from '../SymbolBox/SymbolBox';
-// import DatePicker from '../DatePicker/DatePicker';
-// import Input from '../Input/Input';
-// import Button from '@material-ui/core/Button';
-// import { BaseKeyboardPickerProps } from '@material-ui/pickers/_shared/hooks/useKeyboardPickerState';
-// import axios from 'axios';
-// import querystring from 'querystring';
-// import { TokenContext } from '../../contexts/TokenContext';
-// import currency from 'currency.js';
-// import CandleStickData, { CandleStickQuery } from '../../interfaces/CandleStickData';
-// import Trade from '../../interfaces/Trade';
-
-// export default function BuyBox() {
-//     const token = React.useContext<string>(TokenContext);
-
-    // const { activeInvestment, updateActiveInvestment } = React.useContext(ActiveInvestmentContext);
-//     const { trades, updateTrades } = React.useContext(TradesContext);
-
-//     // useEffect here just to test if trades context working properly
-//     useEffect(() => {
-//         if(trades.items.length > 0) {
-//             console.log(trades.items)
-//         }
-//     })
-
-//     const { stock, from, to } = activeInvestment;
-
-
-    
-//     const getAmountInvested = () => {
-//         return currency(amount);
-//     };
-
-//     // const getBuyInPrice = () => { // we assume perfect market entry, meaning we buy at the lowest price at the start of our investment term
-//     //     return currency(candles?.l[0]!);
-//     // };
-
-//     // const getSellPrice = () => { // we assume perfect exit, meaning we sell at the highest price at the end of our investment term
-//     //     const length = candles?.h.length!;
-//     //     return currency(candles?.h[length-1]!);
-//     // };
-
-    
-    // const updateStartDate: BaseKeyboardPickerProps['onChange'] = async (date) => {
-    //     if (!!date) {
-           
-    //     };      
-    // };
-
-//     const updateEndDate: BaseKeyboardPickerProps['onChange'] = async (date) => {
-//         if (!!date) {
-            
-//             console.log(date)
-
-//         };
-//     };
-
-//     const updateAmount = (event: any) => {
-//         updateActiveInvestment({
-//             ...activeInvestment,
-//             amount: event.target.value
-//         });
-//     };
-
-
-//     const onClickHandlerBuy = async () => {
-//         try {
-//             let trade: Trade = {
-//                 stock,
-//                 date: from,
-//                 price: getBuyInPrice(),
-//                 amount: getAmountInvested(),
-//                 isBuy: true,
-//                 timestamp: moment(),
-//             };
-
-//             updateTrades({
-//                 ...trades,
-//                 items: [trade, ...trades.items]
-//             })
- 
-//         } catch(error) {
-//             errorHandler(error);
-//         };
-//     };
-
-//     const onClickHandlerSell = async () => {
-//         try {
-//             let trade: Trade = {
-//                 stock,
-//                 date: from,
-//                 price: getBuyInPrice(),
-//                 amount: getAmountInvested(),
-//                 isBuy: true,
-//                 timestamp: moment(),
-//             };
-
-//             updateTrades({
-//                 ...trades,
-//                 items: [trade, ...trades.items]
-//             })
- 
-//         } catch(error) {
-//             errorHandler(error);
-//         };
-//     };
-
-   
-   
-
-//     const errorHandler = (error: any) => { // generic error handler
-//         if (error.response?.status === 401) {
-//             console.log("Invalid token, please make sure it is set as react app environment variable");
-//         };
-//         console.error(error);
-//     };
-
-//     // const getShareEstimate = () => {
-//     //     // after the inital data is loaded we take the first candle and use that for our calculation
-//     //     // we assume that we had perfect entry into the market that day and bought at the lowest price available
-//     //     if (!!oneDayCandle) {
-//     //         const price = currency(oneDayCandle['l'][0]).value;
-//     //         const shares = currency(amount).value / price;
-
-//     //         return `${shares.toFixed(4).toString()} shares @ $${price.toString()}`;
-//     //     };
-//     //     return '';
-//     // };
-
-//     return <React.Fragment>
-//         <h2>BuySellBox</h2>
-//         <form>
-//             {/* <Input 
-//                 id="amount" 
-//                 label="Amount" 
-//                 variant="standard"
-//                 inputProps={{
-//                     type: 'number',
-//                     min: '0'
-//                 }}
-//                 helperText={getShareEstimate()}
-//                 adornment="$"
-//                 value={amount}
-//                 onChange={updateAmount}
-//             /> */}
-//             <DatePicker id="startDate" label="Start Date" value={from} onChange={updateStartDate}/>
-//             <Button variant="contained" onClick={onClickHandlerBuy}>Buy</Button>
-//             <DatePicker id="endDate" label="End Date" value={to} onChange={updateEndDate}/>
-//             <Button variant="contained" onClick={onClickHandlerSell}>Sell</Button>
-//         </form>
-//     </React.Fragment>
-// };
