b16e1dcdd62e48353158eb5d579c3ad9af72192a
birdsnbears <louischu15@gmail.com>
2021-06-18 17:30:20 -0700

    Formatted Watchlist. Missing Red/Green Percentages

--- src/components/WatchList/WatchList.tsx
+++ src/components/WatchList/WatchList.tsx
@@ -7,74 +7,56 @@ import moment from 'moment';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import CardHeader from '@material-ui/core/CardHeader';
-import { FixedSizeList, ListChildComponentProps } from 'react-window';
 import WatchListItem from '../WatchListItem/WatchListItem';
-import { makeStyles } from '@material-ui/core/styles';
+
+import "./WatchList.css";
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
 
 export default function WatchList() {
-    const isInit = React.useRef(false);
-    const { watchList, updateWatchList } = React.useContext(WatchListContext);
-    
-    // const cleanUp = () => { // need to think of a way to close this socket
-    //     socket.close();
-    //     console.log('closing socket');
-    // };
+  const isInit = React.useRef(false);
+  const { watchList, updateWatchList } = React.useContext(WatchListContext);
+
+  // const cleanUp = () => { // need to think of a way to close this socket
+  //     socket.close();
+  //     console.log('closing socket');
+  // };
 
 
-    React.useEffect(() => {
-        if (!isInit.current) {
-            console.log('adding listeners to socket');
-            socket.addEventListener('open', () => {
-                socket.addEventListener('message', ({data}) => {
-                    if (!!data && typeof data === 'string') {
-                        const dataObj: WebSocketRawData = JSON.parse(data);
-                        dataObj['data']?.forEach(({s, p, t}) => {
-                            let newWatchList = {...watchList};
-                            newWatchList.stocks[s]['price'] = currency(p);
-                            newWatchList.lastUpdated = moment(t);
-                            updateWatchList(newWatchList);
-                        });
-                    };
-                });
-    
-                Object.keys(watchList.stocks).forEach(subscribe);
+  React.useEffect(() => {
+    if (!isInit.current) {
+      console.log('adding listeners to socket');
+      socket.addEventListener('open', () => {
+        socket.addEventListener('message', ({ data }) => {
+          if (!!data && typeof data === 'string') {
+            const dataObj: WebSocketRawData = JSON.parse(data);
+            dataObj['data']?.forEach(({ s, p, t }) => {
+              let newWatchList = { ...watchList };
+              newWatchList.stocks[s]['price'] = currency(p);
+              newWatchList.lastUpdated = moment(t);
+              updateWatchList(newWatchList);
             });
-        };
-        isInit.current = true;
-    });
+          };
+        });
 
-    // function renderRow(props: ListChildComponentProps) {
-    //     const { index, style } = props;
-        
-    //     return (
-    //         <WatchListItem key={index} style={style} value={watchList[index]}/>
-    //     );
-    // };
-
-    const Row = (props: ListChildComponentProps) => {
-        const { index, style } = props;
-        const data = Object.values(watchList.stocks)[index];
-        return (
-            <WatchListItem key={index} style={style} data={data}></WatchListItem>
-        );
+        Object.keys(watchList.stocks).forEach(subscribe);
+      });
     };
+    isInit.current = true;
+  });
 
-    const useStyles = makeStyles({
-        root: {
-            height: '100%',
-        }
-    });
-
-    const classes = useStyles();
-
-    return <React.Fragment>
-        <Card data-testid="watchlist" className={classes.root}>
-            <CardHeader title="Watchlist"></CardHeader>
-            <CardContent>
-                <FixedSizeList height={400} width={400} itemSize={80} itemCount={Object.keys(watchList.stocks).length}>
-                    {Row}
-                </FixedSizeList>
-            </CardContent>
-        </Card>
-    </React.Fragment>;
+  return <React.Fragment>
+    <Card data-testid="watchlist">
+      <CardHeader title="Watchlist"></CardHeader>
+      <CardContent className="watch-list">
+        <List>
+          {Object.keys(watchList.stocks).map((symbol, index) => {
+            return (<ListItem key={index}>
+              <WatchListItem data={watchList.stocks[symbol]}></WatchListItem>
+            </ListItem>);
+          })}
+        </List>
+      </CardContent>
+    </Card>
+  </React.Fragment>;
 };
--- src/components/WatchListItem/WatchListItem.tsx
+++ src/components/WatchListItem/WatchListItem.tsx
@@ -1,44 +1,24 @@
 import React from 'react';
-import Card, { CardProps } from '@material-ui/core/Card';
 import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
 import { WatchListData } from '../../contexts/WatchListContext';
 
-export interface WatchListItemProps extends CardProps {
-    data: WatchListData;
+import "./WatchListItem.css";
+import { ComponentsProps } from '@material-ui/core/styles/props';
+
+export interface WatchListItemProps extends ComponentsProps {
+  data: WatchListData;
 };
 
 export default function WatchListItem(props: WatchListItemProps) {
-    const { style, data } = props;
-    const useStyles = makeStyles({
-        root: {
-            display: 'flex',
-            justifyContent: 'space-between',
-            alignItems: 'center',
-            '& .symbol': {
-                marginLeft: '1rem'
-            },
-            '& .details': {
-                marginRight: '1rem',
-                '& .dollar': {
-                    textAlign: 'right',
-                },
-                '& .percent': {
-                    textAlign: 'right',
-                }
-            }
+  const { data } = props;
 
-        }
-    });
-    const { root } = useStyles();
-
-    return (
-        <Card data-testid="watchlistitem" style={style} className={root}>
-            <Typography variant="h6" className="symbol">{data.symbol}</Typography>
-            <div className="details">
-                <Typography variant="h6" className="dollar" data-testid="watchlistitem-dollar">{!!data.price ? data.price.format() : '$-'}</Typography>
-                <Typography variant="subtitle2" className="percent" data-testid="watchlistitem-percent">(-%)</Typography>
-            </div>
-        </Card>
-    );
+  return (
+    <div data-testid="watchlistitem" className="list-item">
+      <Typography variant="h6" className="symbol">{data.symbol}</Typography>
+      <div className="details">
+        <Typography variant="subtitle2" className="percent" data-testid="watchlistitem-percent">(-%)</Typography>
+        <Typography variant="h6" className="dollar" data-testid="watchlistitem-dollar">{!!data.price ? data.price.format() : '$-'}</Typography>
+      </div>
+    </div>
+  );
 };
