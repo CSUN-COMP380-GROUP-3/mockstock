17d7d920637844f395448ade36add6905c2bf963
navarrojandg <navarrojanderrick@gmail.com>
2021-06-19 05:18:38 -0700

    Update Stock.ts
    removed timestamp from stock interface

--- src/interfaces/Stock.ts
+++ src/interfaces/Stock.ts
@@ -1,11 +1,9 @@
 import StockSymbolData from './StockSymbolData';
 import currency from 'currency.js';
-import { Moment } from 'moment';
 
 // related to buying or selling
 export default interface Stock {
     stock: StockSymbolData;
     price: currency;
-    shares: number;
-    timestamp: Moment;
+    shares: currency;
 };
