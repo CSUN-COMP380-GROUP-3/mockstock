6670edafa41e620dd7cc038fe8677f41e2ada85e
navarrojandg <navarrojanderrick@gmail.com>
2021-06-19 05:20:50 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,11 @@ import React from 'react';
 import './App.css';
 
 import BuyBox from './components/BuyBox/BuyBox';
-import BuySellBox from './components/BuySellBox/BuySellBox';
-
 import Header from './components/Header/Header';
 import StockChart from './components/StockChart/StockChart';
 import LiquidBalance from './components/LiquidBalance/LiquidBalance';
 import WatchList from './components/WatchList/WatchList';
 import SellBox from './components/SellBox/SellBox';
-import Portfolio from './components/Portfolio/Portfolio';
 
 import { GlobalContext } from './components/GlobalContext/GlobalContext';
 import { WatchListDataContext, WatchListDataInterface } from './contexts/WatchListDataContext';
@@ -24,15 +21,13 @@ function App() {
   return (
     <GlobalContext>
       <Header></Header>
-      <BuyBox></BuyBox>
       <StockChart></StockChart>
       <LiquidBalance></LiquidBalance>
       <SellBox></SellBox>
       <WatchListDataContext.Provider value={watchListDataContextProviderValue}>
         <WatchList></WatchList>
       </WatchListDataContext.Provider>
-      <Portfolio></Portfolio>
-      <BuySellBox></BuySellBox>
+      <BuyBox></BuyBox>
     </GlobalContext>
   );
 }
