abf7f1e81f95f7a053dce936fb394064c74f3373
navarrojandg <navarrojanderrick@gmail.com>
2021-06-23 17:36:09 -0700

    added comments and moved updateLogic to addToPortfolio

--- src/contexts/PortfolioContext.tsx
+++ src/contexts/PortfolioContext.tsx
@@ -10,11 +10,17 @@ export interface PortfolioContextInterface {
     portfolio: PortfolioInterface;
     updatePortfolio: (portfolio: PortfolioInterface) => void;
     has: (symbol: string) => boolean;
-    addToPortfolio: (trade: Trade) => PortfolioInterface;
+    addToPortfolio: (trade: Trade) => boolean;
     length: number;
 };
 
-export class PortfolioProvider implements PortfolioContextInterface {
+/**
+ * PortfolioProvider is meant to be a wrapper around the portfolio state. When the portfolio state 
+ * is created it's reference and its update function are held in the PortfolioProvider's portfolio, 
+ * and updatePortfolio property respectively. Doing so allows all logic pertaining to the portfolio 
+ * to be handled within this exported instance on line 64. 
+ */
+class PortfolioProvider implements PortfolioContextInterface {
     portfolio: PortfolioInterface;
     updatePortfolio: (portfolio: PortfolioInterface) => void;
     constructor() {
@@ -30,21 +36,22 @@ export class PortfolioProvider implements PortfolioContextInterface {
         return !!this.portfolio[symbol] && this.portfolio[symbol].length >= 0;
     };
 
-    addToPortfolio(trade: Trade): PortfolioInterface {
+    addToPortfolio(trade: Trade): boolean {
         // check if the symbol is in the portfolio
         // if has returns true then we have an existing array for the corresponding symbol, 
-        // other wise we need to create one
+        // otherwise we need to create one
         const {stock} = trade;
+        const newPortfolio = Object.assign({}, this.portfolio);
         if (this.has(stock.symbol)) {
             // symbol already in portfolio
             // add most recent trade to the top of the stack
-            this.portfolio[stock.symbol].unshift(trade);
+            newPortfolio[stock.symbol].unshift(trade);
         } else {
             // symbol not in portfolio
-            this.portfolio[stock.symbol] = [trade];
+            newPortfolio[stock.symbol] = [trade];
         };
-
-        return Object.assign({}, this.portfolio);
+        this.updatePortfolio(newPortfolio);
+        return true;
     }; 
 
     /**
