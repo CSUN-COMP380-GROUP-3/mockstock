b4c10162c7ed5f62076ee20e1c089ea04d7f5cdd
MatthewGHub2 <matthew.chalifoux.641@my.csun.edu>
2021-07-02 10:37:40 -0700

    Fixed Portfolio showing empty cards

--- src/components/Portfolio/Portfolio.tsx
+++ src/components/Portfolio/Portfolio.tsx
@@ -1,45 +1,112 @@
 import React from 'react';
-import { PortfolioContext, portfolioProvider } from '../../contexts/PortfolioContext';
+import {
+    PortfolioContext,
+    portfolioProvider,
+} from '../../contexts/PortfolioContext';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import CardHeader from '@material-ui/core/CardHeader';
 import { FixedSizeList, ListChildComponentProps } from 'react-window';
 import PortfolioItem from '../PortfolioItem/PortfolioItem';
 import { makeStyles } from '@material-ui/core/styles';
+import currency from 'currency.js';
+
+export interface ProcessedData {
+    total$Buys: currency;
+    total$Sells: currency;
+    totalBuyShares: currency;
+    totalSellShares: currency;
+}
 
 export default function Portfolio() {
     const portfolio = React.useContext(PortfolioContext);
+    var newdata: object[] = [];
+    for (let i = 0; i < portfolioProvider.length; i++) {
+        const data = Object.values(portfolio)[i];
+        const processTrades = () => {
+            return data.reduce(
+                (acc, curr) => {
+                    const {
+                        total$Buys,
+                        total$Sells,
+                        totalBuyShares,
+                        totalSellShares,
+                    } = acc;
+                    const { type, price, total } = curr;
+                    const newData: ProcessedData = {
+                        total$Buys,
+                        total$Sells,
+                        totalBuyShares,
+                        totalSellShares,
+                    };
+                    const shares = total.divide(price!);
+                    if (type === 'BUY') {
+                        newData.total$Buys = total$Buys.add(total);
+                        newData.totalBuyShares = totalBuyShares.add(shares);
+                    } else {
+                        // SELL
+                        newData.total$Sells = total$Sells.add(total);
+                        newData.totalSellShares = totalSellShares.add(shares);
+                    }
+                    return newData;
+                },
+                {
+                    total$Buys: currency(0),
+                    total$Sells: currency(0),
+                    totalBuyShares: currency(0),
+                    totalSellShares: currency(0),
+                } as ProcessedData,
+            );
+        };
+
+        // (total$Buys - total$Sells) / (totalBuyShares - totalSellShares) // cost basis
+        const { total$Buys, total$Sells, totalBuyShares, totalSellShares } =
+            processTrades();
+        const totalShares = totalBuyShares.subtract(totalSellShares);
+        const costBasis = total$Buys.subtract(total$Sells).divide(totalShares);
+
+        if (totalShares.value === 0) {
+        } else {
+            var dataaa = {
+                costBasis: costBasis,
+                totalShares: totalShares,
+                stock: Object.values(portfolio)[i][0].stock,
+            };
+            newdata.push(dataaa);
+        }
+    }
 
     const Row = (props: ListChildComponentProps) => {
         const { index, style } = props;
 
-        const data = Object.values(portfolio)[index];
+        const data = Object.values(newdata[index]);
 
-        return (
-            <PortfolioItem 
-                key={index} 
-                style={style} 
-                data={data}
-            />
-        );
+        return <PortfolioItem key={index} style={style} data={data} />;
     };
 
     const useStyles = makeStyles({
         root: {
             height: '100%',
-        }
+        },
     });
 
     const classes = useStyles();
 
-    return <React.Fragment>
-        <Card data-testid="portfoliolist" className={classes.root}>
-            <CardHeader title="Portfolio"></CardHeader>
-            <CardContent>
-                <FixedSizeList height={400} width={400} itemSize={80} itemCount={portfolioProvider.length}>
-                    {Row}
-                </FixedSizeList>
-            </CardContent>
-        </Card>
-    </React.Fragment>;
-};
+    return (
+        <React.Fragment>
+            <Card data-testid="portfoliolist" className={classes.root}>
+                <CardHeader title="Portfolio"></CardHeader>
+                <CardContent>
+                    <FixedSizeList
+                        height={400}
+                        width={400}
+                        itemSize={80}
+                        itemCount={newdata.length}
+                    >
+                        {Row}
+                    </FixedSizeList>
+                </CardContent>
+            </Card>
+        </React.Fragment>
+    );
+}
--- src/components/PortfolioItem/PortfolioItem.tsx
+++ src/components/PortfolioItem/PortfolioItem.tsx
@@ -7,20 +7,13 @@ import Trade from '../../interfaces/Trade';
 import currency from 'currency.js';
 
 export interface PortfolioListItemProps extends CardProps {
-    data: Trade[];
-};
-
-export interface ProcessedData {
-    total$Buys: currency;
-    total$Sells: currency;
-    totalBuyShares: currency;
-    totalSellShares: currency;
-};
+    data: Array<Object>[];
+}
 
 export default function PortfolioListItem(props: PortfolioListItemProps) {
     const { style, data } = props;
-    const stock = data[0].stock;
-    const { symbol } = stock;
+    const stock = Object(data[2]);
+    const symbol = Object.values(data[2]);
 
     const useStyles = makeStyles({
         root: {
@@ -28,7 +21,7 @@ export default function PortfolioListItem(props: PortfolioListItemProps) {
             justifyContent: 'space-between',
             alignItems: 'center',
             '& .symbol': {
-                marginLeft: '1rem'
+                marginLeft: '1rem',
             },
             '& .details': {
                 marginRight: '1rem',
@@ -37,10 +30,9 @@ export default function PortfolioListItem(props: PortfolioListItemProps) {
                 },
                 '& .percent': {
                     textAlign: 'right',
-                }
-            }
-
-        }
+                },
+            },
+        },
     });
     const { root } = useStyles();
 
@@ -48,53 +40,33 @@ export default function PortfolioListItem(props: PortfolioListItemProps) {
         activeStockProvider.switchActiveStock(stock);
     };
 
-    const processTrades = () => {
-        return data
-            .reduce((acc, curr) => {
-                const { total$Buys, total$Sells, totalBuyShares, totalSellShares } = acc;
-                const { type, price, total } = curr;
-                const newData: ProcessedData = {
-                    total$Buys,
-                    total$Sells,
-                    totalBuyShares,
-                    totalSellShares,
-                };
-                const shares = total.divide(price!);
-                if (type === 'BUY') {
-                    newData.total$Buys = total$Buys.add(total);
-                    newData.totalBuyShares = totalBuyShares.add(shares);
-                } else {
-                    // SELL
-                    newData.total$Sells = total$Sells.add(total);
-                    newData.totalSellShares = totalSellShares.add(shares);
-                };
-                return newData;
-            }, {
-                total$Buys: currency(0),
-                total$Sells: currency(0),
-                totalBuyShares: currency(0),
-                totalSellShares: currency(0),
-            } as ProcessedData);
-    };
-
-    // (total$Buys - total$Sells) / (totalBuyShares - totalSellShares) // cost basis
-    
-    const { total$Buys, total$Sells, totalBuyShares, totalSellShares } = processTrades();
-    const totalShares = totalBuyShares.subtract(totalSellShares);
-    const costBasis = ( total$Buys.subtract(total$Sells) )
-        .divide(totalShares);
-    
-    console.log(`cost basis ${costBasis}`);
-    const comp = totalShares.value === 0 ?
-         <div></div> : 
-         <Card data-testid="watchlistitem" style={style} className={root} onClick={onClick}>
-            <Typography variant="h6" className="symbol">{symbol}</Typography>
-            <div className="details" >
-                {/* <Typography variant="subtitle2" className="dollar" data-testid="portfolioitem-dollar">{(total$Buys.subtract(total$Sells)).toString()}</Typography> */}
-                <Typography variant="subtitle2" className="cost" data-testid="portfolioitem-cost">{costBasis.toString()}</Typography>
-                <Typography variant="subtitle2" className="shares" data-testid="portfolioitem-shares">{totalShares.format({precision: 4, symbol: ''})}</Typography>
+    const comp = (
+        <Card
+            data-testid="watchlistitem"
+            style={style}
+            className={root}
+            onClick={onClick}
+        >
+            <Typography variant="h6" className="symbol">
+                {symbol[5]}
+            </Typography>
+            <div className="details">
+                <Typography
+                    variant="subtitle2"
+                    className="cost"
+                    data-testid="portfolioitem-cost"
+                >
+                    {data[0].toString()}
+                </Typography>
+                <Typography
+                    variant="subtitle2"
+                    className="shares"
+                    data-testid="portfolioitem-shares"
+                >
+                    {data[1].toString()}
+                </Typography>
             </div>
         </Card>
-
+    );
     return comp;
-};
+}
