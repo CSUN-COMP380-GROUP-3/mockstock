a5af752adb3bd3c997c69c938f7dd85b999e93aa
navarrojandg <navarrojanderrick@gmail.com>
2021-07-06 13:46:21 -0700

    refactored liquidBalance to provider consumer design pattern

--- src/contexts/LiquidBalanceContext.tsx
+++ src/contexts/LiquidBalanceContext.tsx
@@ -1,22 +1,50 @@
 import React from 'react';
 import currency from 'currency.js';
+import storage from '../components/storage';
 
-export interface LiquidBalanceInterface {
-    prev: currency,
-    curr: currency,
+const STORAGE_KEY = 'balance';
+
+export interface LiquidBalanceProviderInterface {
+    balance: number;
+    updateLiquidBalance: (balance: number) => void;
+    add: (balance: number) => void;
+    subtract: (balance: number) => void;
+    balanceAsCurrency: currency;
 };
 
-export interface LiquidBalanceContextInterface {
-    liquidBalance: LiquidBalanceInterface,
-    updateLiquidBalance: (balance: LiquidBalanceInterface) => void
+/**
+ * LiquidBalanceProvider wraps around the balance context and provides useful functions
+ */
+class LiquidBalanceProvider implements LiquidBalanceProviderInterface {
+    balance: number;
+    private previousBalance: number;
+    updateLiquidBalance: (balance: number) => void;
+    constructor() {
+        // we need to verify the storage 
+        this.balance = currency(storage?.getItem(STORAGE_KEY) || process.env.REACT_APP_SEED_MONEY || 100000).value;
+        this.previousBalance = currency(process.env.REACT_APP_SEED_MONEY || 100000).value;
+        this.updateLiquidBalance = () => {};
+    };
+
+    add(balance: number) {
+        const newBalance = currency(this.balance).add(currency(balance));
+        this.updateLiquidBalance(newBalance.value);
+        // sign
+        storage?.setItem(STORAGE_KEY, newBalance.toString());
+    };
+
+    subtract(balance: number) {
+        const newBalance = currency(this.balance).subtract(currency(balance));
+        this.updateLiquidBalance(newBalance.value);
+        // sign
+        storage?.setItem(STORAGE_KEY, newBalance.toString());
+    };
+
+    get balanceAsCurrency() {
+        return currency(this.balance);
+    };
 };
 
-export const initLiquidBalanceContext: LiquidBalanceContextInterface = {
-    liquidBalance: {
-        prev: currency(process.env.REACT_APP_SEED_MONEY || 100000),
-        curr: currency(process.env.REACT_APP_SEED_MONEY || 100000),
-    },
-    updateLiquidBalance: () => {}
-};
+export const liquidBalanceProvider = new LiquidBalanceProvider();
 
-export const LiquidBalanceContext = React.createContext(initLiquidBalanceContext);
+export const LiquidBalanceContext = React.createContext(liquidBalanceProvider.balance);
