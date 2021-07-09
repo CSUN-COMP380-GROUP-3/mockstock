d6bdcda38ddd0685d2b390394f09045a9d440339
navarrojandg <navarrojanderrick@gmail.com>
2021-07-08 17:32:12 -0700

    decoupled LiquidBalance from GlobalContext
    doing so prevents other independent components from rerendering as a result of LiquidBalance

--- src/components/GlobalContext/GlobalContext.tsx
+++ src/components/GlobalContext/GlobalContext.tsx
@@ -2,16 +2,12 @@
 import React from 'react';
 import { TokenContext, TOKEN } from '../../contexts/TokenContext';
 import { StockSymbolsContext, filteredSymbols } from '../../contexts/StockSymbolsContext';
-import { liquidBalanceProvider, LiquidBalanceContext } from '../../contexts/LiquidBalanceContext';
 import { WatchListContext, WatchListTracker, WatchListInterface } from '../../contexts/WatchListContext';
 import { tradesProvider, TradesContext, TradesInterface } from '../../contexts/TradesContext';
 import { activeStockProvider, ActiveStockContext, ActiveStockInterface } from '../../contexts/ActiveStockContext';
 import { portfolioProvider, PortfolioContext, PortfolioInterface } from '../../contexts/PortfolioContext';
 
 export const GlobalContext: React.FC = ({ children }) => {
-    const [liquidBalance, updateLiquidBalance] = React.useState(liquidBalanceProvider.balance);
-    liquidBalanceProvider.balance = liquidBalance;
-    liquidBalanceProvider.updateLiquidBalance = updateLiquidBalance;
 
     const [activeStock, updateActiveStock] = React.useState<ActiveStockInterface>(activeStockProvider.activeStock);
     activeStockProvider.activeStock = activeStock;
@@ -32,15 +28,13 @@ export const GlobalContext: React.FC = ({ children }) => {
         <TokenContext.Provider value={TOKEN}>
             <TradesContext.Provider value={trades}>
                 <StockSymbolsContext.Provider value={filteredSymbols}>
-                    <LiquidBalanceContext.Provider value={liquidBalance}>
-                        <ActiveStockContext.Provider value={activeStock}>
-                            <WatchListContext.Provider value={watchListProviderValue}>
-                                <PortfolioContext.Provider value={portfolio}>
-                                    {children}
-                                </PortfolioContext.Provider>
-                            </WatchListContext.Provider>
-                        </ActiveStockContext.Provider>
-                    </LiquidBalanceContext.Provider>
+                    <ActiveStockContext.Provider value={activeStock}>
+                        <WatchListContext.Provider value={watchListProviderValue}>
+                            <PortfolioContext.Provider value={portfolio}>
+                                {children}
+                            </PortfolioContext.Provider>
+                        </WatchListContext.Provider>
+                    </ActiveStockContext.Provider>
                 </StockSymbolsContext.Provider>
             </TradesContext.Provider>
         </TokenContext.Provider>
