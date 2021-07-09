edddef5edfce3fa224e01321c1d31c339a56789d
navarrojandg <navarrojanderrick@gmail.com>
2021-07-09 05:20:45 -0700

    nuked GlobalContext component off the face of the earth

--- src/App.tsx
+++ src/App.tsx
@@ -8,58 +8,54 @@ import StockChart from './components/StockChart/StockChart';
 import LiquidBalance from './components/LiquidBalance/LiquidBalance';
 import WatchList from './components/WatchList/WatchList';
 import SellBox from './components/SellBox/SellBox';
-
-import { GlobalContext } from './components/GlobalContext/GlobalContext';
 import StockInfo from './components/StockInfo/StockInfo';
 import Portfolio from './components/Portfolio/Portfolio';
 
 function App() {
     return (
-        <GlobalContext>
-            <Grid
-                container
-                alignItems="stretch"
-                spacing={0}
-                className="all-content"
-            >
-                <Grid item xs={8}>
-                    <Grid
-                        container
-                        className="main-content"
-                        direction="column"
-                        alignContent="stretch"
-                        spacing={2}
-                    >
-                        <Grid item>
-                            <Header />
-                        </Grid>
-                        <Grid item>
-                            <StockInfo />
-                        </Grid>
-                        <Grid item>
-                            <StockChart />
-                        </Grid>
-                        <Grid item>
-                            <Portfolio />
-                        </Grid>
+        <Grid
+            container
+            alignItems="stretch"
+            spacing={0}
+            className="all-content"
+        >
+            <Grid item xs={8}>
+                <Grid
+                    container
+                    className="main-content"
+                    direction="column"
+                    alignContent="stretch"
+                    spacing={2}
+                >
+                    <Grid item>
+                        <Header />
                     </Grid>
-                </Grid>
-                <Grid item xs={4} className="side-bar">
-                    <Grid container spacing={2} direction="column">
-                        <Grid item>
-                            <LiquidBalance />
-                        </Grid>
-                        <Grid item>
-                            <WatchList />
-                        </Grid>
-                        <Grid item>
-                            <BuyBox />
-                            <SellBox />
-                        </Grid>
+                    <Grid item>
+                        <StockInfo />
+                    </Grid>
+                    <Grid item>
+                        <StockChart />
+                    </Grid>
+                    <Grid item>
+                        <Portfolio />
                     </Grid>
                 </Grid>
             </Grid>
-        </GlobalContext>
+            <Grid item xs={4} className="side-bar">
+                <Grid container spacing={2} direction="column">
+                    <Grid item>
+                        <LiquidBalance />
+                    </Grid>
+                    <Grid item>
+                        <WatchList />
+                    </Grid>
+                    <Grid item>
+                        <BuyBox />
+                        <SellBox />
+                    </Grid>
+                </Grid>
+            </Grid>
+        </Grid>
     );
 }
 
