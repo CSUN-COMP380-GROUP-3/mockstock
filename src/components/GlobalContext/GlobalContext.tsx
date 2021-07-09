524859eb901c958a2ddb80aa6e52ec69b282051d
navarrojandg <navarrojanderrick@gmail.com>
2021-07-08 18:09:44 -0700

    decoupled TradesContext from GlobalContext and refactored its provider

--- src/components/GlobalContext/GlobalContext.tsx
+++ src/components/GlobalContext/GlobalContext.tsx
@@ -3,7 +3,6 @@ import React from 'react';
 import { TokenContext, TOKEN } from '../../contexts/TokenContext';
 import { StockSymbolsContext, filteredSymbols } from '../../contexts/StockSymbolsContext';
 import { WatchListContext, WatchListTracker, WatchListInterface } from '../../contexts/WatchListContext';
-import { tradesProvider, TradesContext, TradesInterface } from '../../contexts/TradesContext';
 import { activeStockProvider, ActiveStockContext, ActiveStockInterface } from '../../contexts/ActiveStockContext';
 
 export const GlobalContext: React.FC = ({ children }) => {
@@ -13,14 +12,9 @@ export const GlobalContext: React.FC = ({ children }) => {
 
     const [watchList, updateWatchList] = React.useState<WatchListInterface>(WatchListTracker.WatchList);
     const watchListProviderValue = { watchList, updateWatchList };
-    
-    const [trades, updateTrades] = React.useState<TradesInterface>(tradesProvider.trades);
-    tradesProvider.trades = trades;
-    tradesProvider.updateTrades = updateTrades;
 
     return <React.Fragment>
         <TokenContext.Provider value={TOKEN}>
-            <TradesContext.Provider value={trades}>
                 <StockSymbolsContext.Provider value={filteredSymbols}>
                     <ActiveStockContext.Provider value={activeStock}>
                         <WatchListContext.Provider value={watchListProviderValue}>
@@ -28,7 +22,6 @@ export const GlobalContext: React.FC = ({ children }) => {
                         </WatchListContext.Provider>
                     </ActiveStockContext.Provider>
                 </StockSymbolsContext.Provider>
-            </TradesContext.Provider>
         </TokenContext.Provider>
     </React.Fragment>
 };
