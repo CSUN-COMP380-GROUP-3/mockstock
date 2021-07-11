6a295f0de3233ac18e443526adc1b7a7b52eb1be
navarrojandg <navarrojanderrick@gmail.com>
2021-07-11 05:20:32 -0700

    added divider for watchlist item, styled similar to match portfolio

--- src/components/WatchList/WatchList.tsx
+++ src/components/WatchList/WatchList.tsx
@@ -8,6 +8,8 @@ import "./WatchList.css";
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import { v4 as uuid } from 'uuid';
+import Divider from '@material-ui/core/Divider';
+import Typography from '@material-ui/core/Typography';
 
 export default function WatchList() {
 
@@ -21,14 +23,21 @@ export default function WatchList() {
     return <React.Fragment>
         <Card data-testid="watchlist">
             <CardHeader title="Watchlist"/>
-            <CardContent className="watch-list">
-                <List>
-                    {Object.entries(watchList).map(([symbol]) => {
-                        return (<ListItem key={'w'+uuid()}>
-                            <WatchListItem symbol={symbol}></WatchListItem>
-                        </ListItem>);
-                    })}
-                </List>
+            <CardContent>
+                {
+                    Object.keys(watchList).length === 0 ?
+                    <Typography variant="body1" align="center">Your watchlist is empty. Add stocks to your watchlist.</Typography> :
+                    <List className="watch-list">
+                        {Object.entries(watchList).map(([symbol]) => (
+                            <React.Fragment>
+                                <ListItem key={'w'+uuid()}>
+                                    <WatchListItem symbol={symbol}></WatchListItem>
+                                </ListItem>
+                                <Divider />
+                            </React.Fragment>
+                        ))}
+                    </List>
+                }
             </CardContent>
         </Card>
     </React.Fragment>;
