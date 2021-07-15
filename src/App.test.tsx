d0ef5d65c03cf1bf90fbda572a1d7601cc9b6610
navarrojandg <navarrojanderrick@gmail.com>
2021-07-15 14:12:43 -0700

    added black box testing to app

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-xtest('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App Component', () => {
+  it('should render correctly', () => {
+    const { getByTestId } = render(<App />);
+    expect(getByTestId('app')).toBeTruthy();
+  });
 });
